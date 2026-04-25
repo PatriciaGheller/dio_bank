778c5e1cd8f794527525660c5de67098d47ae2e9
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-04-25 11:45:26 -0300

    Configura app.py e wsgi.py com blueprints e db

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,3 +1,6 @@
-from dio_bank.src.app import create_app
+from src.app import create_app
 
+# Cria a aplicação Flask usando a factory
 app = create_app()
+
+# O servidor (gunicorn, uwsgi, etc.) vai usar a variável `app`
